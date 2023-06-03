d17dd09224271a3468efd53771a765f434c17166
refat <khondakar00@gmail.com>
2023-06-02 21:30:06 -0700

    fixing

--- script.js
+++ script.js
@@ -22,6 +22,57 @@ noButton.addEventListener("mouseover", () => {
 
 // Get the "Yes" button element
 const yesButton = document.getElementById("yes-button");
+
+// Add a click event listener to create confetti
 yesButton.addEventListener("click", () => {
-  window.location.href = "yes.html";
+  var confettiElement = document.getElementById("confetti-canvas");
+  var confettiSettings = {
+    target: confettiElement,
+    max: 729,
+    size: 1,
+    animate: true,
+    props: ["circle", "square", "triangle", "line"],
+    colors: [
+      [165, 104, 246],
+      [230, 61, 135],
+      [0, 199, 228],
+      [253, 214, 126],
+    ],
+    clock: 25,
+    rotate: true,
+    start_from_edge: true,
+    respawn: true,
+  };
+
+  yesButton.style.display = "none";
+  noButton.style.display = "none";
+
+  var gif = document.getElementById("gif");
+  var header = document.getElementById("main");
+  header.style.display = "none";
+  gif.style.display = "none";
+
+  //change the style of the confetti canvas
+  confettiElement.style.position = "absolute";
+  confettiElement.style.top = "0";
+  confettiElement.style.left = "0";
+  confettiElement.style.width = "100%";
+  confettiElement.style.height = "100%";
+  confettiElement.style.zIndex = "1000";
+
+  var confetti = new ConfettiGenerator(confettiSettings);
+
+  confetti.render();
+
+  let p = document.createElement("p");
+  p.innerText =
+    "Congrats you made the right choice 🎉 \nDM me the time and venue 😉";
+  p.style.fontSize = "2rem";
+  p.style.fontWeight = "bold";
+  p.style.textAlign = "center";
+  p.style.position = "absolute";
+  p.style.top = "50%";
+  p.style.left = "50%";
+  p.style.transform = "translate(-50%, -50%)";
+  document.body.appendChild(p);
 });
