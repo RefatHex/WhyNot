5e2823de639bc8c50ca266daf458c0070e193963
Iftekharul Islam <71924294+RefatHex@users.noreply.github.com>
2023-12-01 08:50:26 -0700

    Update gif 2

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const noBtn = document.querySelector(".no-btn");
 yesBtn.addEventListener("click", () => {
   question.innerHTML = "Message Me soon!";
   gif.src =
-    "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+    "https://media.giphy.com/media/2WzRsDeKvQUA6XYmiP/giphy.gif";
 });
 
 noBtn.addEventListener("mouseover", () => {
